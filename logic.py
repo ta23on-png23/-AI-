5001323edc54db51d38f389c431365689bca2066
ta23on-png23 <ta23on@gmail.com>
2026-02-01 08:38:15 +0900

    Delete logic.py

--- logic.py
+++
@@ -1,9 +0,0 @@
-def judge_prediction(t1, t4, is_women_race):
-    """
-    展示タイムの差から予測を出すロジック
-    """
-    diff = t1 - t4
-    if diff >= 0.10:
-        return "4-5-1", f"⚠️ 中穴アラート！(タイム差:{diff:.2f})"
-    else:
-        return "1-2-3", "✅ 本命展開"
